060e41aaf9f475eb415248498efa9b73f7fc2f2c
MarkWh1te <iamwh1temark@outlool.com>
2018-11-14 16:59:21 +0800

    fix naming conventition

--- cmd/search/search.go
+++ cmd/search/search.go
@@ -3,29 +3,29 @@ Package search contains everything about search
 */
 package search
 
-func swap(a *int,b *int){
+func swap(a *int, b *int) {
 	tmp := *a
 	*a = *b
 	*b = tmp
 }
 
 // Partition moves the small number to head ,big number to end
-func partition(arr *[]int,l int,r int,n int)(int,int){
-	start := l 
+func Partition(arr *[]int, l int, r int, n int) (int, int) {
+	start := l
 	current := l
 	end := r
 	pivot := (*arr)[n]
-	for current <= end{
-		if (*arr)[current] <  pivot {
-			swap(&(*arr)[current],&(*arr)[start])
+	for current <= end {
+		if (*arr)[current] < pivot {
+			swap(&(*arr)[current], &(*arr)[start])
 			current++
 			start++
 		} else if (*arr)[current] > pivot {
-			swap(&(*arr)[current],&(*arr)[end])
-			end --
+			swap(&(*arr)[current], &(*arr)[end])
+			end--
 		} else if (*arr)[current] == pivot {
-			current ++
+			current++
 		}
 	}
-	return start,end
+	return start, end
 }
--- cmd/search/search_test.go
+++ cmd/search/search_test.go
@@ -1,37 +1,37 @@
 package search
 
 import (
-	"testing"
-	"math/rand"
 	"fmt"
+	"math/rand"
+	"testing"
 )
 
-func random(min int,max int) int{
+func random(min int, max int) int {
 	return rand.Intn(max-min) + min
 }
 
 // TestPartition generate test slice and check it's partition
-func TestPartition(t *testing.T){
+func TestPartition(t *testing.T) {
 	const UPPER int = 1000
 	length := random(10, UPPER)
 	// use slice beacse length is not const
-	test := make([]int,length)
+	test := make([]int, length)
 	for index := 0; index < length; index++ {
-		test[index] = random(1,UPPER)
+		test[index] = random(1, UPPER)
 	}
 	pivot := test[length-1]
-	fmt.Println("pivot: ",pivot)
-	fmt.Println("origin arr: ",test)
-	start,end := partition(&test,0,length-1,length-1)
-	fmt.Println("start index: ",start," end index: ",end)
-	fmt.Println("new arr: ",test)
+	fmt.Println("pivot: ", pivot)
+	fmt.Println("origin arr: ", test)
+	start, end := Partition(&test, 0, length-1, length-1)
+	fmt.Println("start index: ", start, " end index: ", end)
+	fmt.Println("new arr: ", test)
 	for index := 0; index < length; index++ {
-		if test[index] > pivot{
-			if index < end{
+		if test[index] > pivot {
+			if index < end {
 				t.Errorf("big index check error")
 			}
-		} else if test[index] < pivot{
-			if index > start{
+		} else if test[index] < pivot {
+			if index > start {
 				t.Errorf("small index check error")
 			}
 		}
