c716043b31acb296810e71fed31e24ef7f2cafe6
markwh1te <iamwh1temark@outlook.com>
2018-11-19 21:07:05 +0800

    add heap related

--- heap.go
+++ heap.go
@@ -13,7 +13,7 @@ func (heap MaxHeap) parent(i int) int {
 
 // get heap left element
 func (heap MaxHeap) left(i int) int {
-	if (2*i + 1) > heap.size {
+	if (2*i + 1) < heap.size {
 		return 2*i + 1
 	}
 	return 0
@@ -21,21 +21,34 @@ func (heap MaxHeap) left(i int) int {
 
 // get heap right element
 func (heap MaxHeap) right(i int) int {
-	if (2*i + 2) > heap.size {
+	if (2*i + 2) < heap.size {
 		return 2*i + 2
 	}
 	return 0
 }
 
 // Insert element into Maxheap
-func (heap *MaxHeap) Insert(n int) {
-	(*heap).arr = append((*heap).arr, n)
-	(*heap).size++
-}
-
-// Heapify particulry element
-// func (heap *MaxHeap) MaxHeapify(index int) {
-// 	for parent(index) {
-// 		if
-// 	}
+// TODO:fix me !
+// func (heap *MaxHeap) Insert(n int) {
+// 	(*heap).arr = append((*heap).arr, n)
+// 	(*heap).size++
 // }
+
+// MaxHeapify assumes that the binary trees rooted at LEFT and RIGHT are max-heaps,
+// but that node at i might be smaller than its children.
+func (heap *MaxHeap) MaxHeapify(i int) {
+	largest := i
+	r := heap.right(i)
+	l := heap.left(i)
+	fmt.Println(heap.arr[i], heap.arr[l], heap.arr[r], i, l, r, "ffff")
+	if l > 0 && heap.arr[l] > heap.arr[i] {
+		largest = l
+	} else if r > 0 && heap.arr[r] > heap.arr[i] {
+		largest = r
+	}
+	if largest != i {
+		swap(&heap.arr[i], &heap.arr[largest])
+		i = largest
+		heap.MaxHeapify(i)
+	}
+}
--- heap_test.go
+++ heap_test.go
@@ -7,11 +7,15 @@ import (
 
 // TestHeap generate test slice and check it's partition
 func TestHeap(t *testing.T) {
-	const UPPER int = 20
+	const UPPER int = 12
 	test := randomSlice(UPPER)
-	heap := new(MaxHeap)
-	for i := 0; i < len(test); i++ {
-		heap.Insert(test[i])
-	}
-	fmt.Println("the origin heap is: ", heap.arr)
+	var heap MaxHeap
+	heap.arr = test
+	heap.size = len(test)
+	t.Run("basic setting", func(t *testing.T) {
+		heapifyIndex := random(0, heap.size-1)
+		fmt.Println("the origin heap is: ", heap.arr, "\nheapify index is", heapifyIndex)
+		heap.MaxHeapify(heapifyIndex)
+		fmt.Println("the heapify heap is: ", heap.arr)
+	})
 }
