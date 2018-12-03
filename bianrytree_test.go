5a219438db773e1a867874aff5db555cd5f91804
markwh1te <iamwh1temark@outlook.com>
2018-12-03 21:20:50 +0800

    add binarytree start

---
+++ bianrytree_test.go
@@ -0,0 +1,38 @@
+package algo4go
+
+import "testing"
+
+func testMaxDepth(t *testing.T, F BinaryNode) {
+	log(MaxDepth(F))
+}
+
+func testTraversal(t *testing.T, F BinaryNode) {
+	PreOrderResult := []interface{}{"F", "B", "A", "D", "C", "E", "G", "I", "H"}
+	if !testEqual(PreOrderRecursion(F, []interface{}{}), PreOrderResult) {
+		t.Error("pre order traversal fail!")
+	}
+}
+
+func TestBinaryTree(t *testing.T) {
+	// test case from wikipeidia
+	//	https://en.wikipedia.org/wiki/Tree_traversal
+	A := NewBinaryNode("A")
+	B := NewBinaryNode("B")
+	C := NewBinaryNode("C")
+	D := NewBinaryNode("D")
+	E := NewBinaryNode("E")
+	F := NewBinaryNode("F")
+	G := NewBinaryNode("G")
+	H := NewBinaryNode("H")
+	I := NewBinaryNode("I")
+	F.left = &B
+	F.right = &G
+	B.left = &A
+	B.right = &D
+	D.left = &C
+	D.right = &E
+	G.right = &I
+	I.left = &H
+	t.Run("test max depth", func(t *testing.T) { testMaxDepth(t, F) })
+	t.Run("test traversal", func(t *testing.T) { testTraversal(t, F) })
+}
---
+++ binarytree.go
@@ -0,0 +1,39 @@
+package algo4go
+
+// BinaryNode construct binary tree
+type BinaryNode struct {
+	value interface{}
+	left  *BinaryNode
+	right *BinaryNode
+}
+
+// NewBinaryNode construct new binary node with no child
+func NewBinaryNode(v interface{}) BinaryNode {
+	return BinaryNode{value: v, left: nil, right: nil}
+}
+
+// MaxDepth return the max depth of a tree
+func MaxDepth(root BinaryNode) int {
+	if root.left == nil && root.right == nil {
+		return 1
+	}
+	if root.left == nil {
+		return MaxDepth(*root.right) + 1
+	}
+	if root.right == nil {
+		return MaxDepth(*root.left) + 1
+	}
+	return MaxDepth(*root.left) + MaxDepth(*root.right) + 1
+}
+
+// PreOrderRecursion parent node is processed before any of its child nodes is done.
+func PreOrderRecursion(root BinaryNode, results []interface{}) []interface{} {
+	results = append(results, root.value)
+	if root.left != nil {
+		results = PreOrderRecursion(*root.left, results)
+	}
+	if root.right != nil {
+		results = PreOrderRecursion(*root.right, results)
+	}
+	return results
+}
