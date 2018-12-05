6461d03e45f1764a19f23890e72d7f5fffadbb19
markwh1te <iamwh1temark@outlook.com>
2018-12-05 19:40:57 +0800

    add stack and it's test

---
+++ stack_test.go
@@ -0,0 +1,16 @@
+package algo4go
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	s := NewStack()
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+}
