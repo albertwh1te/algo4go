8334c7b2862e43961b86333f4d5dad8274f5cd31
markwh1te <iamwh1temark@outlook.com>
2019-03-27 00:26:42 +0800

    export all package out

---
+++ algo4go.go
@@ -0,0 +1,9 @@
+package algo4go
+
+import (
+	// Export all package out
+	_ "github.com/MarkWh1te/algo4go/binarytree"
+	_ "github.com/MarkWh1te/algo4go/matrix"
+	_ "github.com/MarkWh1te/algo4go/sort"
+	_ "github.com/MarkWh1te/algo4go/stack"
+)
